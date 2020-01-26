35c0354bbd6d65f9994fd12cb5e03fecd7425cb4
Ragnar Sundblad <ragge@kth.se>
2020-01-26 21:20:38 +0100

    added some emacs dir-locals

---
+++ tekfwtool/.dir-locals.el
@@ -0,0 +1,10 @@
+; Indentation settings for C in Emacs
+; tektool.c is written in several different indentation styles, but the
+; style mostly used seems quite close to Emacs "linux" style.
+;((c-mode . ((c-file-style . "linux")
+;	    (subdirs . nil)))
+; )
+(
+ ("tekfwtool.c" . ((c-mode . ((c-file-style . "linux")))))
+ ("target.c" . ((c-mode . ((c-file-style . "linux")))))
+ )
---
+++ tektool/.dir-locals.el
@@ -0,0 +1,7 @@
+; Indentation settings for C in Emacs
+; tektool.c is written in several different indentation styles, but the
+; style mostly used seems quite close to Emacs "linux" style.
+;((c-mode . ((c-file-style . "linux")
+;	    (subdirs . nil)))
+; )
+(("tektool.c" . ((c-mode . ((c-file-style . "linux"))))))
